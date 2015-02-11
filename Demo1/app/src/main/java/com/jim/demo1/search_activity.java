e60017430c9a824599f94a5f546627843b034f6b
Jase Kurasz <jasekurasz@Macintosh.local>
2015-02-11 09:47:16 -0600

    Added a sample auto complete to the search activity. This will be updated with a list of beers from the API

--- Demo1/app/src/main/java/com/jim/demo1/search_activity.java
+++ Demo1/app/src/main/java/com/jim/demo1/search_activity.java
@@ -1,9 +1,17 @@
 package com.jim.demo1;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.TextView;
 
 /**
  * Created by Jim on 2/2/2015.
@@ -16,7 +24,35 @@ public class search_activity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_layout);
+
+        final AutoCompleteTextView beerType = (AutoCompleteTextView) findViewById(R.id.AutoCompleteTextViewType);
+
+
+        //Once the next button is hit on the item keyboard hide the keyboard, jump to the set price function, then move focus to the quantity text field
+        beerType.setOnEditorActionListener(
+                new TextView.OnEditorActionListener() {
+                    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                        boolean handled = false;
+                        if (actionId == EditorInfo.IME_ACTION_NEXT) {
+                            InputMethodManager imm = (InputMethodManager)
+                                    getSystemService(Context.INPUT_METHOD_SERVICE);
+                            imm.hideSoftInputFromWindow(beerType.getWindowToken(), 0);
+                            handled = true;
+                            //request focus of next item
+                        }
+                        return handled;
+                    }
+                });
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,android.R.layout.simple_dropdown_item_1line, BEERS);
+
+        AutoCompleteTextView itemCurrent = (AutoCompleteTextView) findViewById(R.id.AutoCompleteTextViewType);
+
+        itemCurrent.setAdapter(adapter);
+
     }
+
+    static final String[] BEERS = new String[]{"Pilsner", "Stout", "Brown", "IPA", "Cider"};
 }
 
 
