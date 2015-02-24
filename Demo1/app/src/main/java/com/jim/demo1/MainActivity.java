70500985ff7baf18b0b8411697ac054e932f8891
Jase Kurasz <jasekurasz@Macintosh.local>
2015-02-24 11:20:20 -0600

    Added a truster class that will have to be run before connecting to our server, will probably end up putting this in the onCreate method. App will connect to our server now, still working on getting a POST to work

--- Demo1/app/src/main/java/com/jim/demo1/MainActivity.java
+++ Demo1/app/src/main/java/com/jim/demo1/MainActivity.java
@@ -7,19 +7,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-
 import org.json.JSONObject;
-
 import java.util.HashMap;
 import java.util.Map;
-
 
 public class MainActivity extends Activity {
 
@@ -128,9 +124,9 @@ public class MainActivity extends Activity {
 
             @Override
             public void onClick(View arg0) {
-
+                Truster t = new Truster();
+                t.run();
                 postServer();
-
             }
 
         });
@@ -140,7 +136,7 @@ public class MainActivity extends Activity {
     public void postServer() {
 
         Map<String, String> jsonParams = new HashMap<>();
-        jsonParams.put("username", "test1");
+        jsonParams.put("username", "test6");
         jsonParams.put("password", "test");
         String url = "https://140.192.30.230:8443/beertrader/rest/user/createUser";
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
@@ -170,6 +166,36 @@ public class MainActivity extends Activity {
         };
         queue.add(request);
 
+        /*String url = "https://140.192.30.230:8443/beertrader/rest/user/createUser";
+        RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
+        StringRequest sr = new StringRequest(Request.Method.POST,url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("Response", response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Error.Response", error.toString());
+            }
+        }){
+            @Override
+            protected Map<String,String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("username","test6");
+                params.put("password","test");
+
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String,String> headers = new HashMap<>();
+                headers.put("Content-Type", "application/json; charset=utf-8");
+                return headers;
+            }
+        };
+        queue.add(sr);*/
     }
 
 
---
+++ Demo1/app/src/main/java/com/jim/demo1/Truster.java
@@ -0,0 +1,45 @@
+package com.jim.demo1;
+
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+public class Truster {
+
+    public void run() {
+        SSLContext ctx = null;
+        try {
+            ctx = SSLContext.getInstance("TLS");
+        }catch (NoSuchAlgorithmException e) {e.printStackTrace();}
+        try {
+            assert ctx != null;
+            ctx.init(null, new TrustManager[] {
+                    new X509TrustManager() {
+                        public void checkClientTrusted(X509Certificate[] chain, String authType) {}
+                        public void checkServerTrusted(X509Certificate[] chain, String authType) {}
+                        public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[]{}; }
+                    }
+            }, null);
+        } catch (KeyManagementException e) {
+            e.printStackTrace();
+        }
+        HttpsURLConnection.setDefaultSSLSocketFactory(ctx.getSocketFactory());
+        HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+    }
+
+
+
+
+}
