e44af330614a00fa60f28f6131e49553687599d9
Jase Kurasz <jasekurasz@Macintosh-3.local>
2015-04-09 12:09:36 -0500

    Trying to fix Jenkins build

--- app/src/main/java/com/jim/demo1/contact_activity.java
+++ app/src/main/java/com/jim/demo1/contact_activity.java
@@ -2,6 +2,26 @@ package com.jim.demo1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 
 /**
  * Created by Jim on 2/17/2015.
@@ -12,6 +32,9 @@ public class contact_activity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact_layout);
+
+
+
     }
 
 
