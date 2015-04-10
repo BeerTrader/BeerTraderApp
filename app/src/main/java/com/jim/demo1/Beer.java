9a8da8a71301efd4e2761b1fd7e3b7dfb7602425
Jgurgon3 <james_gurgone@yahoo.com>
2015-04-10 12:12:49 -0500

    Post Activity

---
+++ app/src/main/java/com/jim/demo1/Beer.java
@@ -0,0 +1,51 @@
+package com.jim.demo1;
+
+/**
+ * Created by Jim on 4/7/2015.
+ */
+public class Beer {
+    String beer_name;
+    String brewery;
+    String beer_style;
+    String imgUrl;
+
+
+    public Beer(String beer_name, String brewery, String beer_style, String imgUrl){
+        this.beer_name = beer_name;
+        this.brewery = brewery;
+        this.beer_style = beer_style;
+        this.imgUrl = imgUrl;
+    }
+
+    public String getBeer_name() {
+        return beer_name;
+    }
+
+    public void setBeer_name(String beer_name) {
+        this.beer_name = beer_name;
+    }
+
+    public String getBrewery() {
+        return brewery;
+    }
+
+    public void setBrewery(String brewery) {
+        this.brewery = brewery;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getBeer_style() {
+        return beer_style;
+    }
+
+    public void setBeer_style(String beer_style) {
+        this.beer_style = beer_style;
+    }
+}
---
+++ app/src/main/java/com/jim/demo1/RequestSingleton.java
@@ -0,0 +1,69 @@
+package com.jim.demo1;
+
+import android.app.Application;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by Jim on 4/8/2015.
+ */
+public class RequestSingleton extends Application {
+
+    private static RequestSingleton mInstance;
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+    private static Context mCtx;
+
+    private RequestSingleton(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+
+        mImageLoader = new ImageLoader(mRequestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+    }
+
+    public static synchronized RequestSingleton getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new RequestSingleton(context);
+        }
+        return mInstance;
+    }
+
+    public static synchronized  RequestSingleton getInstance() {
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return mImageLoader;
+    }
+}
